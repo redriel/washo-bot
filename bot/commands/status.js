1c09305db9e1d6d368077ec8fc373b111e947fba
Redriel <gb.lavorato@gmail.com>
2020-11-17 21:52:22 +0100

    Added the status command

---
+++ bot/commands/status.js
@@ -0,0 +1,21 @@
+const { name, version, description, author } = require('./../config.json');
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'status',
+    aliases: ['version', 'washo'],
+    description: 'Tell everything about the bot',
+    execute(message, args) {
+
+        const botInfoEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Washo-bot info')
+            .addField('About', `\`${description}\``)
+            .addField('Status', `\`ONLINE\``)
+            .addField('Version', `\`${version}\``)
+            .addField('Author', `\`${author}\``)
+            .setFooter('https://github.com/redriel/washo-bot');
+
+        message.channel.send({ embed: botInfoEmbed });
+    },
+};
